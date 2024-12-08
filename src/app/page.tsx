e1173a5998aed607a008ecd430aef349ae52ef6e
axelsvardh <axel@fragsheet.com>
2024-12-08 22:58:58 +0100

    Clean up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,11 @@
 import React, { Suspense } from 'react';
-// import Image from "next/image";
-// import Link from "next/link";
-// import MatchStatsPopup from '../Overview/Matches_stats_popup';
-// import FocusArea from './focus_areas/page';
-// import Champions from './champions/page';
 import Overview from './Components/';
 import styles from "./page.module.css";
-// import Filters from './Components/filter';
 
 function Page() {
   return (
     <main className={styles.mainContainer}>
-      <Suspense fallback={<div>Loading...</div>}>
         <Overview />
-        {/* <Filters /> */}
-      </Suspense>
     </main>
   );
 }
